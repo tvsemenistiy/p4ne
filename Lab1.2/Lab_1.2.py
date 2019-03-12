977444cec6182c285f9613e8ff15e2b2660758f1
Тимофей Семенистый <timophey.sem1997@gmail.com>
2019-03-12 19:01:22 +0300

    First

---
+++ Lab1.2/Lab_1.2.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+sheet ['A'][1:]
+
+
+def getvalue(x): return x.value
+
+
+list_x = list (map(getvalue,sheet['A'][1:]))
+list_y = list (map(getvalue,sheet['C'][1:]))
+list_y1 = list (map(getvalue,sheet['D'][1:]))
+
+
+pyplot.plot(list_x,list_y,label='Метка')
+pyplot.plot(list_x,list_y1,label='Метка')
+
+pyplot.show()
