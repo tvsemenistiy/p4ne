456a215a6ce626a6675d0055d7cb05df9e6b5163
Тимофей Семенистый <timophey.sem1997@gmail.com>
2019-03-13 18:53:58 +0300

    123

---
+++ Lab1.2/test.py
@@ -0,0 +1,19 @@
+def classify(i):
+if not (i % 2):
+        return "43"
+    elif not (i % 3):
+        return "45"
+    elif not (i % 5):
+        return "46"
+    else:
+        return "42"
+
+i = 1
+
+while i <= 20:
+    j = 1
+    while j <= 20:
+        print("\x1b[%sm%4d\x1b[39m" % \(classify(i * j), i * j), end="")
+      j += 1
+    print("")
+    i += 1
